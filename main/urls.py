a3c63567ee500c3d9c6778a595bfb7845fda48d8
samheartache <20imas07@gmail.com>
2025-03-26 22:33:24 +0300

    Almost finish main app, everything's done except about page text

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from main import views
 app_name = 'main'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about')
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, 'main/index.html', {'title': 'Главная'})
+
+
+def about(request):
+    return render(request, 'main/about.html', {'title': 'О нас'})
