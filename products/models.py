cba9450b8706767672baf31f7dedccf9a7e620bf
samheartache <20imas07@gmail.com>
2025-03-30 15:03:03 +0300

    Add fixtures, optimize all_products page and add models to admin panel

--- products/models.py
+++ products/models.py
@@ -7,10 +7,7 @@ class Product(models.Model):
     description = models.TextField(default='У товара пока нет описания', max_length=500, verbose_name='Описание')
     price = models.DecimalField(default=0.0, max_digits=10, decimal_places=2, verbose_name='Цена')
     image = models.ImageField(upload_to=None, blank=True, null=True, verbose_name='Изображение')
-    category = models.ForeignKey('', verbose_name='Категория', on_delete=models.CASCADE)
-
-    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.CASCADE)
     
     def __str__(self):
         return self.name
@@ -29,6 +26,6 @@ class Category(models.Model):
         return self.name
     
     class Meta:
-        db_tabel = 'category'
-        verbose_name = 'Категория'
+        db_table = 'category'
+        verbose_name = 'Категорию'
         verbose_name_plural = 'Категории'
--- products/views.py
+++ products/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from products.models import Category, Product
 
 def all_products(request):
-    return render(request, 'products/graphicalcards.html')
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    context = {
+        'title': 'Все товары',
+        'categories': categories,
+        'products': products
+    }
+    return render(request, 'products/all_products.html', context)
