c72706033ba7005d7c5e3d2233e2affb8f74f42e
samheartache <20imas07@gmail.com>
2025-03-31 23:58:59 +0300

    Sorting of products was added

---
+++ products/templatetags/products_tags.py
@@ -0,0 +1,10 @@
+from django import template
+from django.utils.http import urlencode
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def proper_url(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update()
+    return urlencode(query)
--- products/views.py
+++ products/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import render, get_list_or_404
 from django.core.paginator import Paginator
 from products.models import Category, Product
 
+
 def get_products(request, category_slug):
+
     page = request.GET.get('page', 1)
+    sort_way = request.GET.get('sort_way', None)
+
     categories = Category.objects.all()
+
     if category_slug == 'all':
         products = Product.objects.all()
     else:
         products = get_list_or_404(Product.objects.filter(category__slug=category_slug))
+    
+    if sort_way and sort_way != 'default':
+        products = products.order_by(sort_way)
+
     paginator = Paginator(products, 6)
     current_page = paginator.page(page)
+
     context = {
         'title': 'Все товары',
         'categories': categories,
@@ -19,8 +29,11 @@ def get_products(request, category_slug):
     }
     return render(request, 'products/products.html', context)
 
+
 def product(request, product_slug):
+
     product = Product.objects.get(slug=product_slug)
+
     context = {
         'title': product.name,
         'product': product
