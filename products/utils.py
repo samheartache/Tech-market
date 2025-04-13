66aa5e53fe51da1998bbb60f37cfcd06ed2b7569
samheartache <20imas07@gmail.com>
2025-04-13 23:59:40 +0300

    Add highlighting to search

--- products/utils.py
+++ products/utils.py
@@ -1,8 +1,13 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery, SearchHeadline
 
 from products.models import Product
 
 
 def query_handler(query):
-    return Product.objects.annotate(search=SearchVector('name', 'description')).filter(search=query)
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
+    result =  Product.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank').filter(rank__gt=0)
+    
+    result = result.annotate(headline=SearchHeadline('name', query, start_sel='<span style="background_color: yellow;"', stop_sel='</span>')
+    return result
