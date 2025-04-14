cdda61745aab5330cf8db3de99effd773bdd284c
samheartache <20imas07@gmail.com>
2025-04-14 21:04:43 +0300

    Redefine user model and apply migrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='user_images', blank=True, null=True, verbose_name='Аватар профиля')
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'Пользователя'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
