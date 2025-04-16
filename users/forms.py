69fbd4b45af324a06b83e03252a65472388fad94
samheartache <20imas07@gmail.com>
2025-04-16 18:25:16 +0300

    Handle incorrect login data input

--- users/forms.py
+++ users/forms.py
@@ -2,12 +2,18 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 
 from users.models import User
+from global_utils.error_messages import INVALID_LOGIN, INACTIVE_ACC
 
 class LoginForm(AuthenticationForm):
     email = forms.EmailField()
     username = forms.CharField()
     password = forms.CharField()
-    
+
+    error_messages = {
+        'invalid_login': INVALID_LOGIN,
+        'inactive': INACTIVE_ACC,
+    }
+
     class Meta:
         model = User
         fields = ['email', 'username', 'password']
