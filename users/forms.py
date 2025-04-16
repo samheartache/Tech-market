42fe6f8e8f97a7782d302a7326a3c3b5689faeea
samheartache <20imas07@gmail.com>
2025-04-16 17:33:58 +0300

    Remove frontend properties from users/forms.py

--- users/forms.py
+++ users/forms.py
@@ -4,18 +4,10 @@ from django.contrib.auth.forms import AuthenticationForm
 from users.models import User
 
 class LoginForm(AuthenticationForm):
-    email = forms.EmailField(
-        widget=forms.TextInput(attrs={'autofocus': True,
-                                      'placeholder': 'Почта',})
-    )
-    username = forms.CharField(
-        widget=forms.TextInput(attrs={'autofocus': True,
-                                      'placeholder': 'Имя пользователя'})
-    )
-    password = forms.CharField(
-        widget=forms.PasswordInput(attrs={'autocomplete': 'current-password',
-                                           'placeholder': 'Пароль',})
-    )
+    email = forms.EmailField()
+    username = forms.CharField()
+    password = forms.CharField()
+    
     class Meta:
         model = User
         fields = ['email', 'username', 'password']
