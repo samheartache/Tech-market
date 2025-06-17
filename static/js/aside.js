bea252dbb6e541d84ad22c127a6cef1cdf2495da
samheartache <20imas07@gmail.com>
2025-06-17 18:09:07 +0300

    Save last states of categories and filters menu using localStorage

--- static/js/aside.js
+++ static/js/aside.js
@@ -1,9 +1,34 @@
-function toggleDropdown(id) {
-    const categoryList = document.getElementById(id);
-    if (categoryList.style.display === 'block') {
-        categoryList.style.display = 'none';
-    } 
-    else {
-        categoryList.style.display = 'block';
+function updateArrow(id) {
+    const dropdown = document.getElementById(id);
+    const arrow = document.getElementById(`${id}-arrow`);
+    if (dropdown && arrow) {
+        arrow.textContent = dropdown.style.display === 'none' ? '▶' : '▼';
     }
 }
+
+
+function toggleDropdown(id) {
+    const dropdown = document.getElementById(id);
+    const isHidden = dropdown.style.display === 'none';
+    
+    dropdown.style.display = isHidden ? 'block' : 'none';
+    localStorage.setItem(`dropdown_${id}_state`, isHidden ? 'open' : 'closed');
+    updateArrow(id);
+}
+
+
+function  restoreDropdownState() {
+    document.querySelectorAll('.dropdown-content').forEach(dropdown => {
+        const id = dropdown.id;
+        const savedState = localStorage.getItem(`dropdown_${id}_state`);
+
+        dropdown.style.display = (savedState === 'closed') ? 'none' : 'block';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    restoreDropdownState();
+    document.querySelectorAll('.dropdown-content').forEach(dropdown => {
+        updateArrow(dropdown.id);
+    });
+});
