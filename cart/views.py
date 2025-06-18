3901d669607e1f08bbb921a57bb0b61f778d537b
samheartache <20imas07@gmail.com>
2025-06-18 22:18:41 +0300

    Add function of decreasing and increasing amount of products in cart

--- cart/views.py
+++ cart/views.py
@@ -14,12 +14,33 @@ def add_to_cart(request, product_slug):
     product = Product.objects.get(slug=product_slug)
 
     if request.user.is_authenticated:
-        Cart.objects.create(user=request.user, product=product, quantity=1)
+        cart_products = Cart.objects.filter(user=request.user, product=product)
+        if cart_products.exists():
+            cart_product = cart_products.first()
+            if cart_product:
+                cart_product.quantity += 1
+                cart_product.save()
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
     return redirect(request.META['HTTP_REFERER'])
 
 
 def remove(request, id_in_cart):
     cart_prod = Cart.objects.get(id=id_in_cart)
     cart_prod.delete()
+    return redirect(request.META['HTTP_REFERER'])
+
+
+def decrease(request, product_slug):
+    product = Product.objects.get(slug=product_slug)
+
+    if request.user.is_authenticated:
+        cart_products = Cart.objects.filter(user=request.user, product=product)
+        cart_product = cart_products.first()
+        if cart_product.quantity > 1:
+            cart_product.quantity -= 1
+            cart_product.save()
+        else:
+            Cart.objects.get(user=request.user, product=product).delete()
     return redirect(request.META['HTTP_REFERER'])
 
