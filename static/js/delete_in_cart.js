bd5456aad36716a99f9bd2f4850c61aff4edd6b8
samheartache <20imas07@gmail.com>
2025-06-20 23:33:54 +0300

    Implement simple function of deleting product from cart, without recounting price yet

---
+++ static/js/delete_in_cart.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(document).on("click", ".remove_from_cart", function (e) {
+        e.preventDefault();
+        const product_id = $(this).data("product-id")
+        const remove_url = $(this).attr("href");
+
+        $.ajax({
+            type: "POST",
+            url: remove_url,
+            data: {
+                product_id: product_id,
+                csrfmiddlewaretoken: $("[name=csrfmiddlewaretoken]").val(),
+            },
+            success: function (data) {
+                const cartBlock = document.getElementById(`${product_id}`)
+                cartBlock.remove()
+            }
+        })
+
+    })
+})
