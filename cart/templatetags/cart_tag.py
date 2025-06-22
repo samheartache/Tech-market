cd2c1f4ab164b1dc55558ab0ab0a3a95fdd8955e
samheartache <20imas07@gmail.com>
2025-06-23 02:01:20 +0300

    Fix bug with wrong total price display in cart

--- cart/templatetags/cart_tag.py
+++ cart/templatetags/cart_tag.py
@@ -1,6 +1,6 @@
 from django import template
 
-from cart.utils import get_user_cart, all_in_order, amount_in_order
+from cart.utils import get_user_cart, all_in_order, amount_in_order, price_in_order
 
 register = template.Library()
 
@@ -17,3 +17,7 @@ def all_in_order_tag(request):
 @register.simple_tag()
 def amount_in_order_tag(request):
     return amount_in_order(request=request)
+
+@register.simple_tag()
+def price_in_order_tag(request):
+    return price_in_order(request=request)
