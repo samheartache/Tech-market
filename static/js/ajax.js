f051086c836e17b2bf8a51dcb982357dbc62c36d
samheartache <20imas07@gmail.com>
2025-06-23 15:10:39 +0300

    Fix bug with csrf_token and remove notifications of interactions with cart

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,25 +1,4 @@
 $(document).ready(function() {
-    function showNotification(message, isError = false) {
-        const notification = $("#jq-notification");
-        notification.stop(true, true)
-            .removeClass('fade-out')
-            .html(message)
-            .css('opacity', 1)
-            .show();
-        
-        if (isError) {
-            notification.css('background-color', '#f44336');
-        } else {
-            notification.css('background-color', '#4CAF50');
-        }
-        
-        setTimeout(function() {
-            notification.addClass('fade-out');
-            setTimeout(function() {
-                notification.hide();
-            }, 400);
-        }, 1500);
-    }
 
     $(document).on("click", ".add_to_cart, .remove_from_cart", function(e) {
         e.preventDefault();
@@ -42,25 +21,12 @@ $(document).ready(function() {
                     button.removeClass("add_to_cart").addClass("remove_from_cart");
                     button.text("Удалить из корзины");
                     button.attr("href", button.data("remove-url"));
-                    showNotification(data.message);
                 } else {
                     button.removeClass("remove_from_cart").addClass("add_to_cart");
                     button.text("Добавить в корзину");
                     button.attr("href", button.data("add-url"));
-                    showNotification(data.message, true);
                 }
-            },
-            error: function(xhr) {
-                const errorMsg = isAddAction 
-                    ? "Ошибка при добавлении товара" 
-                    : "Ошибка при удалении товара";
-                showNotification(errorMsg, true);
-                console.error(xhr.responseText);
             }
         });
-    });
-    
-    $(window).scroll(function() {
-        $("#jq-notification").css('top', 20 + $(window).scrollTop() + 'px');
     });
 });
