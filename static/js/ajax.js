bbe0f462b75d5aa3cbe3b99be1087a14bb50ecc9
samheartache <20imas07@gmail.com>
2025-06-23 15:26:19 +0300

    Bug with adding and removing products on the main catalog page is now fully fixed

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,31 +1,33 @@
 $(document).ready(function() {
-
-    $(document).on("click", ".add_to_cart, .remove_from_cart", function(e) {
+    $(document).on("submit", ".cart-form", function(e) {
         e.preventDefault();
         
-        const button = $(this);
-        const product_id = button.data("product-id");
-        const csrf_token = $("[name=csrfmiddlewaretoken]").val();
+        const form = $(this);
+        const button = form.find("button[type='submit']");
+        const product_id = form.find("input[name='product_id']").val();
+        const csrf_token = form.find("input[name='csrfmiddlewaretoken']").val();
         const isAddAction = button.hasClass("add_to_cart");
-        const url = isAddAction ? button.data("add-url") : button.data("remove-url");
 
         $.ajax({
             type: "POST",
-            url: url,
+            url: form.attr("action"),
             data: {
                 product_id: product_id,
-                csrfmiddlewaretoken: csrf_token,
+                csrfmiddlewaretoken: csrf_token
             },
             success: function(data) {
                 if (isAddAction) {
+                    form.attr("action", "/cart/remove/");
                     button.removeClass("add_to_cart").addClass("remove_from_cart");
                     button.text("Удалить из корзины");
-                    button.attr("href", button.data("remove-url"));
                 } else {
+                    form.attr("action", "/cart/add_to_cart/");
                     button.removeClass("remove_from_cart").addClass("add_to_cart");
                     button.text("Добавить в корзину");
-                    button.attr("href", button.data("add-url"));
                 }
+            },
+            error: function(xhr) {
+                console.error("Ошибка:", xhr.responseText);
             }
         });
     });
