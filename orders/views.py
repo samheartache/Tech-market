42e12919bcc43da68c17f193a4669599fa1a47f1
samheartache <20imas07@gmail.com>
2025-06-25 23:47:36 +0300

    View for get request to order page is done

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
+from orders.forms import OrderForm
+
 def order(request):
+    if request.method == 'POST':
+        form = OrderForm(data=request.POST)
+        if form.is_valid():
+            ...
+    else:
+        initial = {
+            'first_name': request.user.first_name,
+            'last_name': request.user.last_name,
+            'email': request.user.email,
+            'phone_number': request.user.phone_number
+        }
+
+        form = OrderForm(initial=initial)
+    
     context = {
-        'title': 'Оформление заказа'
+        'title': 'Оформление заказа',
+        'form': form,
     }
     return render(request, 'order.html', context=context)
+
