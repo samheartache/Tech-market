e6d1424cc86d79f7a13ff3f4c506c396c17e1a01
samheartache <20imas07@gmail.com>
2025-06-30 15:27:04 +0300

    First step of rearranging project with class based views, main app is done

--- main/views.py
+++ main/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 
-def index(request):
-    return render(request, 'main/index.html', {'title': 'Главная'})
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная'
+        return context
 
 
-def about(request):
-    return render(request, 'main/about.html', {'title': 'О нас'})
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'О нас'
+        return context
+
