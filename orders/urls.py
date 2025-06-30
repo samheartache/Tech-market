d944b9e6aab7851c97ce0e6972090e00a9521f1a
samheartache <20imas07@gmail.com>
2025-07-01 02:43:33 +0300

    Class based views were added to orders app

--- orders/urls.py
+++ orders/urls.py
@@ -4,8 +4,8 @@ from orders import views
 app_name = 'orders'
 
 urlpatterns = [
-    path('order/', views.order, name='order'),
-    path('userorders/', views.user_orders, name='userorders'),
-    path('cancel/', views.cancel, name='cancel'),
-    path('repeat/', views.repeat, name='repeat'),
+    path('order/', views.MakeOrderView.as_view(), name='order'),
+    path('userorders/', views.UserOrdersView.as_view(), name='userorders'),
+    path('cancel/', views.CancelOrderView.as_view(), name='cancel'),
+    path('repeat/', views.RepeatOrderView.as_view(), name='repeat'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,118 +1,124 @@
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.forms import ValidationError
 from django.db import transaction
 from django.utils import timezone
 from django.db.models import Prefetch
+from django.views.generic import FormView, TemplateView
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from orders.forms import OrderForm
 from orders.utils import get_order_items
 from orders.models import Order, OrderProduct
 from users.models import User
 
-def order(request):
-    if request.method == 'POST':
-        form = OrderForm(data=request.POST)
-        if form.is_valid():
-            try:
-                with transaction.atomic():
-                    user = request.user
-                    user_order = get_order_items(request=request)
-                    phone_number = form.cleaned_data['phone_number']
-                    first_name = form.cleaned_data['first_name']
-                    last_name = form.cleaned_data['last_name']
-                    user_db = User.objects.filter(id=request.user.id)
+
+class MakeOrderView(LoginRequiredMixin, FormView):
+    template_name = 'order.html'
+    form_class = OrderForm
+    success_url = reverse_lazy('user:account')
+
+    def get_initial(self):
+        initial = super().get_initial()
+        initial['first_name'] = self.request.user.first_name
+        initial['last_name'] = self.request.user.last_name
+        initial['email'] = self.request.user.email
+        initial['phone_number'] = self.request.user.phone_number
+        return initial
+    
+    def form_valid(self, form):
+        try:
+            with transaction.atomic():
+                user = self.request.user
+                user_order = get_order_items(request=self.request)
+                phone_number = form.cleaned_data['phone_number']
+                first_name = form.cleaned_data['first_name']
+                last_name = form.cleaned_data['last_name']
+                user_db = User.objects.filter(id=self.request.user.id)
 
 
-                    if user_order.exists():
-                        order = Order.objects.create(
-                            user=user,
-                            phone_number=phone_number,
-                            requires_delivery=form.cleaned_data['requires_delivery'],
-                            delivery_address=form.cleaned_data['delivery_address'],
-                            payment_on_get=form.cleaned_data['payment_on_get'],
+                if user_order.exists():
+                    order = Order.objects.create(
+                        user=user,
+                        phone_number=phone_number,
+                        requires_delivery=form.cleaned_data['requires_delivery'],
+                        delivery_address=form.cleaned_data['delivery_address'],
+                        payment_on_get=form.cleaned_data['payment_on_get'],
+                    )
+
+                    for prod in user_order:
+                        product = prod.product
+                        name = prod.product.name
+                        price = prod.product.price
+                        quantity = prod.quantity
+
+                        OrderProduct.objects.create(
+                            order=order,
+                            product=product,
+                            name=name,
+                            price=price,
+                            quantity=quantity,
                         )
+                
+                if user.phone_number != phone_number:
+                    user_db.update(phone_number=phone_number)
+                
+                if user.first_name != first_name:
+                    user_db.update(first_name=first_name)
+                
+                if user.last_name != last_name:
+                    user_db.update(last_name=last_name)
+                
+                user_order.delete()
 
-                        for prod in user_order:
-                            product = prod.product
-                            name = prod.product.name
-                            price = prod.product.price
-                            quantity = prod.quantity
-
-                            OrderProduct.objects.create(
-                                order=order,
-                                product=product,
-                                name=name,
-                                price=price,
-                                quantity=quantity,
-                            )
-                    
-                    if user.phone_number != phone_number:
-                        user_db.update(phone_number=phone_number)
-                    
-                    if user.first_name != first_name:
-                        user_db.update(first_name=first_name)
-                    
-                    if user.last_name != last_name:
-                        user_db.update(last_name=last_name)
-                    
-                    user_order.delete()
-
-                    return redirect('user:account')
-            except ValidationError as e:
+                return redirect('user:account')
+            
+        except ValidationError as e:
                 return redirect('orders:order')
-    else:
-        initial = {
-            'first_name': request.user.first_name,
-            'last_name': request.user.last_name,
-            'email': request.user.email,
-            'phone_number': request.user.phone_number
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Оформление заказа'
+        return context
+
+
+class UserOrdersView(TemplateView):
+    template_name = 'user_orders.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Ваши заказы'
+        context['orders'] = Order.objects.filter(user=self.request.user.id).prefetch_related(Prefetch("orderproduct_set", queryset=OrderProduct.objects.select_related('product')))
+        return context
+
+
+class CancelOrderView(View):
+    def post(self, request):
+        order_id = request.POST.get('order_id')
+        order = Order.objects.get(id=order_id)
+
+        order.status ='Отменён'
+        order.cancellation_date = timezone.now()
+        order.save()
+
+        response = {
+            "cancellation_date": order.cancellation_date.strftime("%d.%m.%Y %H:%M")
+        }
+        return JsonResponse(response)
+
+
+class RepeatOrderView(View):
+    def post(self, request):
+        order_id = request.POST.get('order_id')
+        order = Order.objects.get(id=order_id)
+
+        order.status = 'В обработке'
+        order.save()
+
+        response = {
+            "delivery_date": "(функция для подсчета времени доставки)",
         }
 
-        form = OrderForm(initial=initial)
-    
-    context = {
-        'title': 'Оформление заказа',
-        'form': form,
-    }
-    return render(request, 'order.html', context=context)
-
-
-def user_orders(request):
-    orders = Order.objects.filter(user=request.user.id).prefetch_related(Prefetch("orderproduct_set", queryset=OrderProduct.objects.select_related('product')))
-
-    context = {
-        'title': 'Ваши заказы',
-        'orders': orders
-    }
-
-    return render(request, 'user_orders.html', context=context)
-
-
-def cancel(request):
-    order_id = request.POST.get('order_id')
-    order = Order.objects.get(id=order_id)
-
-    order.status ='Отменён'
-    order.cancellation_date = timezone.now()
-    order.save()
-
-    response = {
-        "cancellation_date": order.cancellation_date.strftime("%d.%m.%Y %H:%M")
-    }
-    return JsonResponse(response)
-
-
-def repeat(request):
-    order_id = request.POST.get('order_id')
-    order = Order.objects.get(id=order_id)
-
-    order.status = 'В обработке'
-    order.save()
-
-    response = {
-        "delivery_date": "(функция для подсчета времени доставки)",
-    }
-
-    return JsonResponse(response)
-
+        return JsonResponse(response)
